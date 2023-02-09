261e6d5be9072d4913011b89fbfe803217a8bc3b
Poipurin <b.pinorojo@gmail.com>
2023-02-08 21:18:43 -0300

    Calculadora

---
+++ assets/js/calculadora.js
@@ -0,0 +1,17 @@
+"use strict"
+const display = document.querySelector("#display");
+const botones = document.querySelectorAll("button");
+
+botones.forEach((btn) => {
+    btn.addEventListener("click", () =>{
+        if(btn.id === "=") {
+            display.value = eval(display.value);
+        }else if (btn.id === "ac"){
+            display.value = "";
+        }else if (btn.id == "de"){
+            display.value = display.value.slice(0, -1);
+        }else {
+            display.value += btn.id;
+        }
+    });
+});
